fec5f0a18929161cf7730aafe92c870bfdbdea26
Abdül Samed Topcu <topcu.abdulsamet@hotmail.com>
2023-08-31 14:06:29 +0300

    .

--- app/src/main/java/com/example/countryinfo/MainActivity.java
+++ app/src/main/java/com/example/countryinfo/MainActivity.java
@@ -11,6 +11,8 @@ import com.example.countryinfo.databinding.ActivityCountryinfoBinding;
 import com.example.countryinfo.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,13 +27,13 @@ public class MainActivity extends AppCompatActivity {
         countryArrayList=new ArrayList<>();
 
         //veriler - kendimiz yazdık
-        country almanya=new country("Almanya","Berlin",R.drawable.almanyabayrakinfo,R.drawable.almanyainfo);
-        country brezilya=new country("Brezilya","Brazilya",R.drawable.brezilyabayrak,R.drawable.brezilyainfo);
-        country arjantin=new country("Arjantin","Buenos Aires",R.drawable.arjantinbayrak,R.drawable.arjantininfo);
-        country amerika=new country("Amerika","Washington DC",R.drawable.amerikabayrak,R.drawable.amerikainfo);
-        country avustralya=new country("Avustralya","Kanberra",R.drawable.avustralyabayrak,R.drawable.avustralyainfo);
-        country belcika=new country("Belçika","Brüksel",R.drawable.belcikabayrak,R.drawable.belcikainfo);
-        country birlesikkrallik=new country("Birleşik Krallık","Londra",R.drawable.kingtomflag,R.drawable.kingtmmap);
+        country almanya=new country("Almanya","Berlin",R.drawable.almanyaflag,R.drawable.almanyamap);
+        country brezilya=new country("Brezilya","Brazilya",R.drawable.brezilyaflag,R.drawable.brezilyamap);
+        country arjantin=new country("Arjantin","Buenos Aires",R.drawable.arjantinflag,R.drawable.arjantinmap);
+        country amerika=new country("Amerika","Washington DC",R.drawable.amerikaflag,R.drawable.amerikamap);
+        country avustralya=new country("Avustralya","Kanberra",R.drawable.avustralyaflag,R.drawable.avustralyamap);
+        country belcika=new country("Belçika","Brüksel",R.drawable.belcikaflag,R.drawable.belcikamap);
+        country birlesikkrallik=new country("Birleşik Krallık","Londra",R.drawable.birlesikkralikflag,R.drawable.birlesikkralikmap);
         country çin=new country("Çin","Pekin",R.drawable.cinflag,R.drawable.cinmap);
         country danimarka=new country("Danimarka","Kopenhag",R.drawable.danimarkaflag,R.drawable.danimarkamap);
         country ekvador=new country("Ekvador","Qutio",R.drawable.ekvadorflag,R.drawable.ekvadormap);
@@ -44,9 +46,9 @@ public class MainActivity extends AppCompatActivity {
         country gurcıstan=new country("Gürcistan","Tiflis",R.drawable.gurcistanflag,R.drawable.gurcistanmap);
         country hirvatistan=new country("Hırvatistan","Zagreb",R.drawable.hirvatistanflag,R.drawable.hirvatistanmap);
         country hindistan=new country("Hindistan","Yeni Delhi",R.drawable.hindistanflag,R.drawable.hindistanmap);
-        country hollanda=new country("Hollanda","Amsterdam", R.drawable.almanyabayrakinfo,R.drawable.hollandamap);
+        country hollanda=new country("Hollanda","Amsterdam", R.drawable.hollandaflag,R.drawable.hollandamap);
         country ırak=new country("Irak","Bağdat",R.drawable.irakflag,R.drawable.irakmap);
-        country iran=new country("İran","Tahran", R.drawable.almanyabayrakinfo,R.drawable.iranmap);
+        country iran=new country("İran","Tahran", R.drawable.iranflag,R.drawable.iranmap);
         country irlanda=new country("İrlanda","Dublin",R.drawable.irlandaflag,R.drawable.irlandamap);
         country italya=new country("İtalya","Roma",R.drawable.italyaflag,R.drawable.italyamap);
 
@@ -75,7 +77,15 @@ public class MainActivity extends AppCompatActivity {
         countryArrayList.add(iran);
         countryArrayList.add(irlanda);
         countryArrayList.add(italya);
+        Comparator<country> nameComparator = new Comparator<country>() {
+            @Override
+            public int compare(country country1, country country2) {
+                return country1.name.compareTo(country2.name);
+            }
+        };
 
+        // Comparator kullanarak ArrayList'i sırala
+        Collections.sort(countryArrayList, nameComparator);
 
 
         /*ArrayAdapter arrayAdapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,
